4a9c0a8f6a7826859da4a6594ef4d1d9bb7c081c
UmarMaqbool <umarmaqbool37@gmail.com>
2025-01-07 18:59:15 +0500

    Quick fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 
 const App = () => {
   // const env = "http://localhost:5002";
-  const env = "https://beta.engageraise.com";
+  // const env = 'https://beta.engageraise.com';
+  const env = 'https://secure.engageraise.com';
 
+
+  // U2FsdGVkX1/6UlkmTbFHBTztOpq89xZIq+FW/plK8o/2gj4Nvz0HUXBlCVGDsl+QbVU/e5x8xHabjx3vAyPnaSE3fG+BYoMakI3WY+jPEhU=
   const [email, setEmail] = useState("umarmaqbool37@gmail.com");
-  const [password, setPassword] = useState("Test1234");
 
   const handleRedirectWithApi = async () => {
     let xApiKey = "",
@@ -14,15 +16,21 @@ const App = () => {
       actionPageUrl = "";
     if (env === "http://localhost:5002") {
       xApiKey =
-        "U2FsdGVkX19dkFeUXdq6hjCEMRK3ZStmV5ZH13GRtACndTnSMuf2ex5yVMHfVuVT";
+        "U2FsdGVkX1+BA98Q11fyRlFy6AhETlatjIEMoj0JpbJbsj8WqlONDr4axaqRFZR3";
       tokenUrl = "http://localhost:5002/v1/auth/generate_session_token";
-      actionPageUrl = "http://localhost:3000/donate/client-action-page-test";
-    } else {
+      actionPageUrl = 'http://localhost:3000/donate/client-action-page-test';
+    } else if(env === 'https://beta.engageraise.com') {
       xApiKey =
         "U2FsdGVkX19XUJ8rCVkI+tv1bmmaTGXlB0OZXgTOF5hVw/GNKOG8ezLEvIPZGfnVApvbakj9FFpx7yZA7xiG2g==";
       tokenUrl =
         "https://api.beta.engageraise.com/v1/auth/generate_session_token";
       actionPageUrl = `${env}/donate/test-action-page-1`;
+    } else {
+      xApiKey =
+      "U2FsdGVkX1/6UlkmTbFHBTztOpq89xZIq+FW/plK8o/2gj4Nvz0HUXBlCVGDsl+QbVU/e5x8xHabjx3vAyPnaSE3fG+BYoMakI3WY+jPEhU=";
+      tokenUrl =
+        "https://api.secure.engageraise.com/v1/auth/generate_session_token";
+      actionPageUrl = `${env}/donate/abe-lincoln-test`;
     }
     try {
       const response = await fetch(tokenUrl, {
@@ -31,7 +39,7 @@ const App = () => {
           "x-api-key": xApiKey,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email }),
       });
 
       if (!response.ok) {
@@ -56,14 +64,10 @@ const App = () => {
       <div>
         <label>Email: </label>
         <input
-          type='email'
+          type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-      </div>
-      <div>
-        <label>Password: </label>
-        <input value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
       <button onClick={handleRedirectWithApi}>Redirect with Token</button>
     </div>
